f168145085346b516f07ee2e9926a5ffc0a0662f
Miguel A. Yanez <may2114@columbia.edu>
2015-11-24 16:52:49 -0500

    Cleans up AMiner2CSV

--- AMiner2CSV.py
+++ AMiner2CSV.py
@@ -1,12 +1,29 @@
-import unicodecsv as csv
-import re
-import sys
-import pandas as pd
-from sets import Set
+"""Usage:
+   AMiner2CSV.py paper_file author_file author2paper_file
 
-def parsePapersToCSV(papers_file):
-	dblp_papers = open("dblp_papers.csv", "w")
-	dblp_ref = open("dblp_ref.csv", "w")
+"""
+import unicodecsv as csv
+import time
+from sets import Set
+from tqdm import *
+from docopt import docopt
+
+PAPERS_OUT = "dblp_papers.csv"
+REF_OUT = "dblp_ref.csv"
+AUTHOR_OUT = "dblp_author.csv"
+AFF_OUT = "dblp_aff.csv"
+A2A_OUT = "dblp_author2aff.csv"
+A2P_OUT = "dblp_author2paper.csv"
+
+def parsePapersToCSV(papers_filename):
+	
+	global PAPERS_OUT
+	global REF_OUT
+
+	papers_file = open(papers_filename, 'r')
+
+	dblp_papers = open(PAPERS_OUT, "w")
+	dblp_ref = open(REF_OUT, "w")
 
 	paper_wr = csv.writer(dblp_papers, quoting=csv.QUOTE_ALL)
 	ref_wr = csv.writer(dblp_ref)
@@ -14,7 +31,7 @@ def parsePapersToCSV(papers_file):
 	INDEX = ""
 	row = []
 	ref_line = []
-	for rline in papers_file:
+	for rline in tqdm(papers_file):
 		line = rline.decode("utf-8").rstrip().replace("\n","").replace("\"", "").replace('\\', "")
 		if line.startswith("#index"):
 			INDEX = line.lstrip("#index")
@@ -45,16 +62,20 @@ def parsePapersToCSV(papers_file):
 
 	dblp_papers.close()
 	dblp_ref.close()
+	papers_file.close()
 
-def parseAuthorToCSV(author_file):
+def parseAuthorToCSV(author_filename):
 	
-	dblp_authors = open("dblp_author.csv","w")
+	global AUTHOR_OUT
+
+	author_file = open(author_filename, 'r')
+	dblp_authors = open(AUTHOR_OUT,"w")
 
 	author_wr = csv.writer(dblp_authors,quoting=csv.QUOTE_ALL)
 
 	row = []
 
-	for rline in author_file:
+	for rline in tqdm(author_file):
 		line = rline.decode("utf-8").rstrip().replace("\n","")
 
 		if line.startswith("#index"):
@@ -73,15 +94,19 @@ def parseAuthorToCSV(author_file):
 			row = []
 
 	dblp_authors.close()
+	author_file.close()
 
-def parseAffiliationsToCSV(aff_file):
+def parseAffiliationsToCSV(aff_filename):
 
-	dblp_aff = open("dblp_aff.csv","w")
+	global AFF_OUT
+
+	aff_file = open(aff_filename, 'r')
+	dblp_aff = open(AFF_OUT,"w")
 	aff_wr = csv.writer(dblp_aff, quoting=csv.QUOTE_ALL)
 
 	aff_set = Set()
 
-	for rline in aff_file:
+	for rline in tqdm(aff_file):
 		line = rline.decode("utf-8").rstrip().replace("\n","")
 
 		if line.startswith("#index"):
@@ -100,25 +125,31 @@ def parseAffiliationsToCSV(aff_file):
 
 	AFF_INDEX = 0
 
-	for aff in aff_set:
+	for aff in tqdm(aff_set):
 		aff_wr.writerow([AFF_INDEX,aff])
 		AFF_INDEX += 1
 
 	dblp_aff.close()
+	aff_file.close()
 
-def parseAuthorAffiliationsToCSV(authors_file, aff_file):
-	aff_df = csv.reader(open(aff_file, 'r'))
-	authors_df = open(authors_file, 'r')
+def parseAuthorAffiliationsToCSV(authors_filename, aff_filename):
+	
+	global A2A_OUT
+
+	aff_file = open(aff_filename, 'r')
+	authors_file = open(authors_filename, 'r')
+
+	aff_df = csv.reader(aff_file)
 	aff_map = {}
 
-	dblp_aa = open("dblp_author2affiliation.csv", "w")
+	dblp_aa = open(A2A_OUT, "w")
 	aa_wr = csv.writer(dblp_aa)
 
 	for a in aff_df:
 		aff_map[a[1]] = a[0]
 
 	INDEX = ""
-	for rline in authors_df:
+	for rline in tqdm(authors_file):
 		line = rline.decode("utf-8").rstrip().replace("\n","")
 
 		if line.startswith("#index"):
@@ -136,16 +167,37 @@ def parseAuthorAffiliationsToCSV(authors_file, aff_file):
 			continue
 
 	dblp_aa.close()
+	aff_file.close()
+	authors_file.close()
+
+def parseAuthor2PaperToCSV(author2paper_filename):
+	
+	global A2P_OUT
+
+	a2p_file = open(author2paper_filename, 'r')
+	a2p_out = open(A2A_OUT, "w")
+
+	for line in tqdm(a2p_file):
+		a2p_out.write(line.replace("\t",','))
+
+	a2p_file.close()
+	a2p_out.close()
+	
 
 if __name__ == "__main__":
-	papers = open(sys.argv[1], 'r')
-	parsePapersToCSV(papers)
+	args = docopt(__doc__)
 
-	authors = open(sys.argv[2], 'r')
-	parseAuthorToCSV(authors)
+	papers_file = args['paper_file']
+	author_file = args['author_file']
+	a2p_file = args['author2paper_file']
 
-	authors = open(sys.argv[2], 'r')
-	parseAffiliationsToCSV(authors)
-
-
-	parseAuthorAffiliationsToCSV(sys.argv[2], "dblp_aff.csv")
+	print "Papers2CSV"
+	parsePapersToCSV(papers_file)
+	print "Author2CSV"
+	parseAuthorToCSV(author_file)
+	print "Aff2CSV"
+	parseAffiliationsToCSV(author_file)
+	print "Author2AffCSV"
+	parseAuthorAffiliationsToCSV(author_file, AFF_OUT)
+	print "Author2PaperCSV"
+	parseAuthor2PaperToCSV(a2p_file)
