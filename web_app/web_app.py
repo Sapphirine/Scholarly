5ac082e24cd56acb2e4ccc6441b4662402bdfebe
Miguel A. Yanez <may2114@columbia.edu>
2015-12-18 13:35:59 -0500

    Adds Multithreaded Flask

--- web_app/web_app.py
+++ web_app/web_app.py
@@ -121,5 +121,5 @@ def get_top(category, limit):
 		return json.dumps(result)
  
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=8888)
+	app.run(host='0.0.0.0', port=8888, threaded=True)
 
